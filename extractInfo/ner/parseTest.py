bdf7c182e6cce0a690196455d307f97ca8faa1e1
besperk93 <barneyperkins93@gmail.com>
2022-03-28 19:00:34 +0100

    update 28/03 - housekeeping

--- extractInfo/ner/parseTest.py
+++ extractInfo/ner/parseTest.py
@@ -1,6 +1,8 @@
 import infoExtractor
 
 script_location = "Vault/testScripts"
+output_location = "Vault/graph"
 
 extractor = infoExtractor.infoExtractor(script_location)
 extractor.open_transcript()
+extractor.display_entity_data()
